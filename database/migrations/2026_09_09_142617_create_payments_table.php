399ef971c47738c0ef9bdfbcda31c71720f2df1f
Bob <bob@example.com>
2026-09-16 23:12:34 +0200

    ajout de la table des paiement

---
+++ database/migrations/2026_09_09_142617_create_payments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        
+Schema::create('payments', function (Blueprint $table) {
+    $table->id();
+    $table->unsignedBigInteger('user_id');
+    $table->string('stripe_payment_id')->nullable();
+    $table->integer('amount'); // en centimes
+    $table->string('currency')->default('eur');
+    $table->string('status')->default('pending'); // pending, succeeded, failed
+    $table->text('description')->nullable();
+    $table->timestamps();
+    
+    $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+});
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payments');
+    }
+};
